8b61c7ed14b75bf74816f359adccf43fd5b5a6e6
Stantmn <shaidurovos@gmail.com>
2017-04-04 19:15:20 +0300

    add page component

--- appfront/src/app/app.module.ts
+++ appfront/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
