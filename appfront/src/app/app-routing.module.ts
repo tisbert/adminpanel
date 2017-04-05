85d8eddc7dae674ff1f8ae30a8812bf05d4c6fd9
Stantmn <shaidurovos@gmail.com>
2017-04-05 21:06:42 +0300

    SB admin theme implemented

---
+++ appfront/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', loadChildren: './components/layout/layout.module#LayoutModule' }
+    // { path: 'login', loadChildren: './login/login.module#LoginModule' },
+    // { path: 'signup', loadChildren: './signup/signup.module#SignupModule' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- appfront/src/app/app.module.ts
+++ appfront/src/app/app.module.ts
@@ -3,20 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageComponent } from './components/page/page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
