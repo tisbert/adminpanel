ac37cd0019347501fdacb5aa77fa0e51b62a6d64
Stantmn <shaidurovos@gmail.com>
2017-04-13 22:57:32 +0300

    fixed TinyMCE

--- appfront/src/app/app.module.ts
+++ appfront/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { TinymceModule } from 'angular2-tinymce';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +14,7 @@ import { AppComponent } from './app.component';
         BrowserModule,
         FormsModule,
         HttpModule,
-        AppRoutingModule,
-        TinymceModule.withConfig({})
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
